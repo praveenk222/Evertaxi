26d0fd672a74d2ee1ba8c168aec79f97dc7ad819
praveenk222 <praveenkadma222@gmail.com>
2023-12-25 10:13:10 +0530

    ddd

---
+++ Evertax.API_SQL/app/controllers/admin/Admin/admin.router.js
@@ -0,0 +1,44 @@
+var  express = require('express');
+const fs=require('fs');
+var  router = express.Router();
+var  Db = require('./admin.service');
+
+
+router.use((request, response, next) => {
+    next();
+  });
+router.route('/get').get((request, response) => {
+    Db.getProducts().then((data) => {
+      response.json(data[0]);
+    })
+  })
+router.route('/getPriceData').get((request, response) => {
+    Db.getProductTime().then((data) => {
+      response.status(200).json(data[0]);
+    })
+  })
+  
+  router.route('/get/:id').get((request, response) => {
+    console.log(request.params.id)
+    Db.getProductByID(request.params.id).then((data) => {
+      response.json(data);
+    })
+  })
+  
+  router.route('/bybranch/:id').get((request, response) => {
+    console.log(request.params.id)
+    Db.getProductByBranchID(request.params.id).then((data) => {
+      response.json(data);
+    })
+  })
+  
+  router.route('/products').post((request, response) => {
+    let  member = { ...request.body }
+    Db.addMember(member).then(data  => {
+      response.status(201).json(data);
+    })
+  })
+
+    
+
+module.exports = router;
